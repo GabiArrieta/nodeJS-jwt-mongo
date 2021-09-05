6aad21020452fd907299fe92551df25d171b4778
gabiarrieta <gabriela.arrieta.187@gmail.com>
2021-09-05 00:00:03 -0300

    modify package.json and .env

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import app from './app';
+require('dotenv').config();
 import './database';
 
-app.listen(4000);
-console.log('server listen on port', 4000);
+app.listen(process.env.PORT || 3000);
+console.log('server listen on port', process.env.PORT);
